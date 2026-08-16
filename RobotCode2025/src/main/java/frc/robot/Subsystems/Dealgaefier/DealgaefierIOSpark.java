d5a2389d253ef1117ca51511a5fd5f6d8a87b223
Michelle <158527456+Stellay2976@users.noreply.github.com>
2026-08-15 23:36:36 -0400

    more code w/o comments

--- RobotCode2025/src/main/java/frc/robot/Subsystems/Dealgaefier/DealgaefierIOSpark.java
+++ RobotCode2025/src/main/java/frc/robot/Subsystems/Dealgaefier/DealgaefierIOSpark.java
@@ -25,4 +25,84 @@ import edu.wpi.first.wpilibj.DutyCycleEncoder;
 public class DealgaefierIOSpark implements DealgaefierIO
 {
     final SparkMax deployMotor = new SparkMax(DealgaefierConstants.kDeployCanID, MotorType.kBrushless);
+    final RelativeEncoder deployEncoder = deployMotor.getEncoder();
+
+    final SparkMax intakeMotor = new SparkMax(DealgaefierConstants.kIntakeCanID, MotorType.kBrushless);
+    final RelativeEncoder intakeEncoder = intakeMotor.getEncoder();
+
+    public DutyCycleEncoder deployAbsEncoder = new DutyCycleEncoder(DealgaefierConstants.kDeployAbsEncoder);
+
+    private LoggedNetworkNumber tuningP = new LoggedNetworkNumber("/Tuning/Dealgaefier/P", DealgaefierConstants.kProportionalGainSpark);
+    private LoggedNetworkNumber tuningD = new LoggedNetworkNumber("/Tuning/Dealgaefier/D", DealgaefierConstants.kDerivativeTermSpark);
+    private LoggedNetworkNumber tuningG = new LoggedNetworkNumber("/Tuning/Dealgaefier/G", DealgaefierConstants.kGravityTermSpark);
+
+    @AutoLogOutput (key="Dealgaefier/Setpoint")
+    private double absSetpoint;
+
+    private PIDController controller = new PIDController(DealgaefierConstants.kProportionalGainSpark, DealgaefierConstants.kIntegralTermSpark, DealgaefierConstants.kDerivativeTermSpark);
+
+    private SparkMaxConfig deployConfig;
+
+    @AutoLogOutput (key="Dealgaefier/DesiredVoltage")
+    private double desiredVoltage = 0.0;
+
+    public boolean absEncoderInitialized = false;
+
+    @AutoLogOutput (key="Dealgaefier/Deployed")
+    public boolean deployed = false;
+
+    public DealgaefierIOSpark()
+    {
+        this.controller.enableContinuousInput(0,1);
+
+        deployConfig = new SparkMaxConfig();
+        deployConfig
+            .idleMode(IdleMode.kBrake)
+            .smartCurrentLimit(DealgaefierConstants.kCurrentLimit)
+            .voltageCompensation(12);
+        
+        tryUntilOk(
+            deployMotor,
+            5,
+            () -> deployMotor.configure(deployConfig, ResetMode.kResetSafeParameters,
+                    PersistMode.kPersistParameters));
+        
+        var intakeConfig = new SparkMaxConfig().apply(deployConfig);
+        intakeConfig.smartCurrentLimit(DealgaefierConstants.kCurrentLimit);
+
+        intakeConfig.inverted(true);
+        tryUntilOk(
+            intakeMotor,
+            5,
+            () -> intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters,
+                    PersistMode.kPersistParameters));
+
+        this.controller.setTolearnce(kTolerance); //Doesn't actually do anything unless using: "controller.atSetpoint()"
+    }
+
+    @Override
+    public void updateInputs(DealgaefierIOInputs inputs)
+    {
+        ifOk(deployMotor, deployEncoder::getPosition, (value) -> inputs.deployMotorPositionRotations = value);
+        ifOk(deployMotor, deployEncoder::getVelocity, (value) -> inputs.deployMotorVelocityRadPerSec = value);
+        ifOk(
+            deployMotor,
+            new DoubleSupplier[] {deployMotor::getAppliedOutput, deployMotor::getBusVoltage},
+            (values -> inputs.deployMotorAppliedVolts = values[0] * values[1]));
+        ifOk(deployMotor, deployMotor::getOutputCurrent,(value) -> inputs.deployMotorCurrentAmps = value);
+
+
+        ifOk(intakeMotor, intakeEncoder::getPosition, (value) -> inputs.intakeMotorPositionRotations = value);
+        ifOk(intakeMotor, intakeEncoder::getVelocity, (value) -> inputs.intakeMotorVelocityRadPerSec = value);
+        ifOk(
+            intakeMotor,
+            new DoubleSupplier[] {intakeMotor::getAppliedOutput, intakeMotor::getBusVoltage},
+            (values -> inputs.deployMotorAppliedVolts = values[0] * values [1]));
+        ifOk(intakeMotor, intakeMotor::getOutputCurrent, (value) -> inputs.intakeMotorCurrentAmps = value);
+
+        if(absEncoderInitialized == false)
+        {
+            initializeDutyEncoder();
+        }
+    }
 }
